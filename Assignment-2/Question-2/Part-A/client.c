f5df7594a950d60cc7e92b82be1036ef63b56d90
adityaaggupta2017 <adityaaggupta2017@gmail.com>
2024-09-27 16:15:43 +0530

    commit_main

---
+++ Assignment-2/Question-2/Part-A/client.c
@@ -0,0 +1,50 @@
+#include "client.h"
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  const char *request = "Get Top CPU-Consuming Processes";
+  char buffer[BUFFER_SIZE] = {0};
+
+  // Create socket
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("Socket creation error");
+    return -1;
+  }
+
+  // Server address setup
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  // Convert IP address from text to binary form
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    perror("Invalid address/Address not supported");
+    return -1;
+  }
+
+  // Connect to the server
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Connection failed");
+    return -1;
+  }
+
+  // Send request for top CPU-consuming processes
+  send(sock, request, strlen(request), 0);
+  printf("Request sent to server: %s\n", request);
+
+  // Receive the response from the server
+  int valread = read(sock, buffer, BUFFER_SIZE);
+  if (valread > 0) {
+    printf("Server reply: %s\n", buffer);
+  } else {
+    printf("Failed to read response from server.\n");
+  }
+
+  // Close the socket
+  close(sock);
+
+  return 0;
+}
---
+++ Assignment-2/Question-2/Part-A/server.c
@@ -0,0 +1,123 @@
+#include "server.h"
+#define PORT 8080
+
+
+void get_processes(Process process_stored[2]) {
+    DIR *process_directory; // directory to store the information about the processes
+    struct dirent *proc_entry;
+
+    char starting_path[512];
+    char buffer[1024] = {0};
+    int pid;
+    Process current_proc;
+
+    memset(process_stored, 0, 2 * sizeof(Process)); // Correct memory initialization
+
+    process_directory = opendir("/proc"); // Open the /proc directory
+
+    if (!process_directory) {
+        perror("Failed to opendir!");
+        return;
+    }
+
+    while (1) {
+        proc_entry = readdir(process_directory);
+
+        if (proc_entry == NULL) {
+            break; // End of directory
+        }
+
+        if (isdigit(*proc_entry->d_name)) { // Only process directories with numerical names (i.e., PID)
+            pid = atoi(proc_entry->d_name);
+
+            // Construct path of the form /proc/[pid]/stat
+            snprintf(starting_path, sizeof(starting_path), "/proc/%d/stat", pid);
+
+            int fd1 = open(starting_path, O_RDONLY); // Read the /proc/[pid]/stat file
+            if (fd1 < 0) {
+                continue;
+            }
+
+            read(fd1, buffer, sizeof(buffer)); // Store the result in the buffer
+            close(fd1);
+
+            // Parse process information from the stat file
+            sscanf(buffer,
+                   "%d %s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu",
+                   &current_proc.pid, current_proc.name_proc, &current_proc.utime, &current_proc.stime);
+
+            unsigned long total_time = current_proc.utime + current_proc.stime;
+
+            if (total_time > process_stored[0].utime + process_stored[0].stime) {
+                process_stored[1] = process_stored[0]; // Shift top process to second
+                process_stored[0] = current_proc; // Set new top process
+            } else if (total_time > process_stored[1].utime + process_stored[1].stime) {
+                process_stored[1] = current_proc; // Set new second top process
+            }
+        }
+    }
+
+    closedir(process_directory);
+}
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connection accepted from %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+    // Receive the client's message
+    int read_val = read(new_socket, buffer, 1024);
+    if (read_val > 0) {
+        printf("Received request from client: %s\n", buffer);
+    }
+
+    // Get top CPU-consuming processes
+    Process process_stored[2];
+    get_processes(process_stored);
+
+    // Prepare the response with process details
+    snprintf(buffer, sizeof(buffer), 
+             "Top 2 CPU-consuming processes are as follows:\n"
+             "1. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n"
+             "2. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n", 
+             process_stored[0].pid, process_stored[0].name_proc, process_stored[0].utime, process_stored[0].stime,
+             process_stored[1].pid, process_stored[1].name_proc, process_stored[1].utime, process_stored[1].stime);
+
+    // Send the process info to the client
+    send(new_socket, buffer, strlen(buffer), 0);
+    printf("Response sent to client with process details\n");
+
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
