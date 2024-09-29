c875dcd23168da4c041039a41720ef9b4f3afdd0
adityaaggupta2017 <adityaaggupta2017@gmail.com>
2024-09-29 00:45:35 +0000

    commit_VM_CLIENT

--- Assignment-2/Question-2/Part-C/client.c
+++ Assignment-2/Question-2/Part-C/client.c
@@ -21,7 +21,7 @@ int main() {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, "192.168.160.48", &serv_addr.sin_addr) <= 0) {
 
     perror("Invalid address/ Address not supported");
 
